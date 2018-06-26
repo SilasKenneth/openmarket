79133c49889d7f4e66975c05d874670be6f9ecc4
Silas Kenneth <Silas Kenneth>
2018-06-26 14:44:28 +0300

    Added more admin functionality like changing password and viewing profile

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from passlib.apps import custom_app_context as pwd_context
 from model import *
 import random
+
 app = Flask(__name__)
 
 sign_up_error = []
@@ -12,12 +13,16 @@ errors_signup = {
     "name": "Name cannot be empty"
 }
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+
 def any_empty(fields):
     sign_up_error = []
     for field in fields:
         if fields[str(field).strip()].strip() == "":
             sign_up_error.append(field)
     return sign_up_error
+
+
 @app.route("/")
 def index():
     # alp = [chr(c) for c in range(65, 91)]
@@ -33,7 +38,9 @@ def index():
     # db_session.commit()
     users = None
     return render_template("index.html", users=users)
-@app.route("/trader/login", methods=["POST","GET"])
+
+
+@app.route("/trader/login", methods=["POST", "GET"])
 def trader_login():
     old = OrderedDict()
     old['username'] = ""
@@ -48,11 +55,14 @@ def trader_login():
         return render_template("traders/login.html", old=old)
     else:
         return render_template("traders/login.html", old=old)
-@app.route("/trader/signup", methods=["POST","GET"])
+
+
+@app.route("/trader/signup", methods=["POST", "GET"])
 def trader_signup():
     counties = County.query.all()
     old = OrderedDict()
-    sample = [('idnum', '32281737'), ('county', "Migori"),('phone', '0791350402'), ('name', 'Silas Kenneth'), ('email', 'silaskenneth1@gmail.com')]
+    sample = [('idnum', '32281737'), ('county', "Migori"), ('phone', '0791350402'), ('name', 'Silas Kenneth'),
+              ('email', 'silaskenneth1@gmail.com')]
     for k in range(len(sample)):
         sample[k] = list(sample[k])
         old[sample[k][0]] = ""
@@ -60,8 +70,9 @@ def trader_signup():
         for kk in range(len(sample)):
             old[sample[kk][0]] = request.form[sample[kk][0]]
         try:
-            trader = Trader(request.form['name'],request.form['email'],request.form['county'],request.form['idnum'],request.form['phone']
-                            ,request.form['password'])
+            trader = Trader(request.form['name'], request.form['email'], request.form['county'], request.form['idnum'],
+                            request.form['phone']
+                            , request.form['password'])
 
             db_session.add(trader)
             db_session.commit()
@@ -70,9 +81,13 @@ def trader_signup():
         except Exception as ex:
             print(ex)
             db_session.rollback()
-        return render_template("traders/new.html", errors = [] if len(any_empty(request.form)) == 0 else any_empty(request.form), old = old, counties=counties)
+        return render_template("traders/new.html",
+                               errors=[] if len(any_empty(request.form)) == 0 else any_empty(request.form), old=old,
+                               counties=counties)
     else:
-        return render_template("traders/new.html", old = old, counties=counties)
+        return render_template("traders/new.html", old=old, counties=counties)
+
+
 @app.route("/trader/changepass")
 def trader_changepass():
     if 'username' not in session:
@@ -83,6 +98,21 @@ def trader_changepass():
         return render_template("traders/changepass.html")
 
 
+@app.route("/logout")
+@app.route('/logout/')
+def logout():
+    if len(session) == 0:
+        return redirect(url_for('index'))
+    else:
+        session['user'] = None
+        if session['user_type'] == 'admin':
+            session['user_type'] = None
+            session.clear()
+            return redirect(url_for('admin_login'))
+        else:
+            session['user_type'] = None
+            session.clear()
+            return redirect(url_for('index'))
 
 
 @app.route("/admin")
@@ -98,6 +128,38 @@ def admin_index():
             return redirect(url_for("admin_login"))
     else:
         return redirect(url_for('admin_login'))
+
+
+@app.route("/admin/account", methods=['GET', 'POST'])
+@app.route("/admin/account", methods=['GET', 'POST'])
+def admin_prof():
+    if 'user' in session:
+        if session['user'] is None:
+            return redirect(url_for("admin_login"))
+        else:
+            if session['user_type'] == 'admin':
+                return render_template('admin/profile.html')
+            else:
+                return redirect(url_for('admin_login'))
+    else:
+        return redirect(url_for('admin_login'))
+
+
+@app.route("/admin/changepass", methods=['GET', 'POST'])
+@app.route("/admin/changepass", methods=['GET', 'POST'])
+def admin_change_pass():
+    if 'user' in session:
+        if session['user'] is None:
+            return redirect(url_for("admin_login"))
+        else:
+            if session['user_type'] == 'admin':
+                return render_template('admin/changepass.html')
+            else:
+                return redirect(url_for('admin_login'))
+    else:
+        return redirect(url_for('admin_login'))
+
+
 @app.route("/admin/login/", methods=['POST', 'GET'])
 @app.route("/admin/login/", methods=['POST', 'GET'])
 def admin_login():
@@ -109,74 +171,103 @@ def admin_login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
+        old['password'] = password
+        old['username'] = username.strip()
         if password.strip() != "" and username.strip() != "":
-            old['password'] = password
-            old['username'] = username
-            creds = db_session.query(Admin).filter(Admin.username == username).one_or_none()
-            print(creds)
-            if creds == None:
+            old['password'] = password.strip()
+            old['username'] = username.strip()
+            creds = db_session.query(Admin).filter(Admin.username == username or Admin.email == username).one_or_none()
+            if creds is None:
                 error_wrong = "Wrong username or password"
-                return render_template("admin/login.html", error=error_wrong)
+                return render_template("admin/login.html", error=error_wrong, old=old)
             else:
-                return render_template("admin/login.html")
+                verified = creds.verify_password(password)
+                user = OrderedDict()
+                user['display_name'] = creds.username
+                user['username'] = username
+                user['email'] = creds.email
+                user['id'] = creds.id
+                session['user_type'] = 'admin'
+                session['user'] = user
+                if verified:
+                    return redirect(url_for("admin_index"))
+                error_wrong = "Wrong username or password"
+                return render_template("admin/login.html", old=old, error=error_wrong)
         else:
             error_empty = "Please specify a username and a password!"
-            return render_template("admin/login.html", error=error_empty)
+            return render_template("admin/login.html", error=error_empty, old=old)
     return render_template("admin/login.html", old=old)
 
 
 @app.route("/trader")
 def trader_home():
     return render_template("traders/index.html")
+
+
 @app.route("/traders/<int:ids>")
 def view_trader(ids):
     trader = db_session.query(Trader).filter(Trader.id == ids).one_or_none()
     if trader == None:
         return render_template("traders/404.html")
     county = db_session.query(County).filter(County.id == trader.county).one_or_none()
-    return render_template("traders/profile.html", trader = trader, county=county)
-#Livestock route
+    return render_template("traders/profile.html", trader=trader, county=county)
+
+
+# Livestock route
 @app.route("/livestocks/<int:ids>")
 def view_livestock(ids):
     live = db_session.query(Livestock).filter(Livestock.id == ids).one_or_none()
     if live == None:
-        return render_template("livestocks/404.html", animal = ids)
-    return render_template("livestocks/profile.html", livestock = live)
+        return render_template("livestocks/404.html", animal=ids)
+    return render_template("livestocks/profile.html", livestock=live)
+
+
 @app.route("/livestocks/<int:ids>/edit")
 def edit_livestock(ids):
     live = db_session.query(Livestock).filter(Livestock.id == ids).one_or_none()
     if live == None:
         return render_template("livestocks/404.html")
     trader = live.traders
-    return render_template("livestocks/edit.html", live = live, trader = trader)
-@app.route("/livestocks/<int:ids>/delete", methods = ["POST", "GET"])
+    return render_template("livestocks/edit.html", live=live, trader=trader)
+
+
+@app.route("/livestocks/<int:ids>/delete", methods=["POST", "GET"])
 def delete_livestock(ids):
     return render_template("livestocks/delete.html")
+
+
 @app.route("/livestocks/<int:ids>/photoupload", methods=["GET", "POST"])
 def livestock_photoupload(ids):
     return render_template("livestocks/photoupload.html")
+
+
 @app.route("/livestocks")
 def livestock_index():
     livestocks = db_session.query(Livestock).all()
     return render_template("livestocks/index.html", livestocks=livestocks)
 
+
 # Medications route
 
-@app.route("/livestocks/<int:ids>/medications", methods = ["GET", "POST"])
+@app.route("/livestocks/<int:ids>/medications", methods=["GET", "POST"])
 def medications(ids):
     animal = db_session.query(Livestock).filter(Livestock.id == ids).one_or_none()
     if animal == None:
         return render_template("livestocks/404.html")
     medications = animal.medications
     diseases = animal.diagnosis
-    return render_template("medications/index.html", animal = animal, medications=medications, diseases = diseases)
+    return render_template("medications/index.html", animal=animal, medications=medications, diseases=diseases)
+
+
 @app.route("/livestocks/<int:ids>/medications/new")
 def create_medication(ids):
     return render_template("medications/new.html")
+
+
 @app.route("/livestocks/<int:ids>/medications/<int:ids1>")
 def view_medication(ids, ids1):
     return render_template("medications/view.html")
 
 
 if __name__ == "__main__":
-    app.run(host="localhost",port=3000,debug=True)
+    app.run(host="localhost", port=3000, debug=True)
--- model.py
+++ model.py
@@ -3,11 +3,15 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, REAL
 from passlib.apps import custom_app_context as pwd_context
+from passlib.hash import pbkdf2_sha256
+
 engine = create_engine("sqlite:///db/begin.db", convert_unicode=True)
 metadata = MetaData()
-db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
+
 class Admin(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
@@ -15,43 +19,55 @@ class Admin(Base):
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(80), nullable=False)
     profile_pic = Column(String(50), nullable=False, default="/static/profiles/profile.png")
-    def __init__(self, email, username,password,profile_pic):
+
+    def __init__(self, email, username, password, profile_pic=None):
         self.email = email
         self.username = username
         self.password = password
         self.profile_pic = profile_pic
-    def hash_password(self, password):
-        self.password = pwd_context.encrypt(password)
+
+    def hash_password(self):
+        self.password = pbkdf2_sha256.hash(self.password)
+
     def verify_password(self, password):
-        return pwd_context.verify(self.password, password)
+        return pbkdf2_sha256.verify(password, self.password)
+
+
 class Trader(Base):
     __tablename__ = 'traders'
     id = Column(Integer, primary_key=True)
     id_pass = Column(Integer, unique=True, nullable=False)
-    name = Column(String(50),unique=True)
-    email = Column(String(150),unique=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(150), unique=True)
     phone = Column(String(60), unique=True)
     bio = Column(String(500), nullable=False, default="No bio availbale")
     county = Column(Integer)
     password = Column(String(128), nullable=False)
     livestocks = relationship("Livestock", back_populates="traders")
-    profile_pic = Column(String(250), nullable=False,default="/static/profiles/profile.jpg")
-    def __init__(self, name=None, email=None, county=1, id_pass=None, phone=None, password = None):
-        self.name = name 
+    profile_pic = Column(String(250), nullable=False, default="/static/profiles/profile.jpg")
+
+    def __init__(self, name=None, email=None, county=1, id_pass=None, phone=None, password=None):
+        self.name = name
         self.email = email
         self.county = county
         self.id_pass = id_pass
         self.phone = phone
         self.password = password
+
     def hash_password(self, password):
         self.password = pwd_context.encrypt(password)
+
     def verify_password(self, password):
         return pwd_context.verify(self.password, password)
+
+
 class Type(Base):
     __tablename__ = "types"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
     default_image = Column(String(200), unique=True)
+
+
 class Livestock(Base):
     __tablename__ = "livestocks"
     id = Column(Integer, primary_key=True)
@@ -63,6 +79,8 @@ class Livestock(Base):
     medications = relationship("Medication", back_populates='livestocks')
     types = relationship("Type")
     profile_pic = Column(String(120), nullable=False)
+
+
 class Diagnosis(Base):
     __tablename__ = "diagnosis"
     id = Column(Integer, primary_key=True)
@@ -71,21 +89,28 @@ class Diagnosis(Base):
     disease_id = Column(Integer, ForeignKey("diseases.id"))
     livestocks = relationship("Livestock")
     visits = relationship("Visit", back_populates="diagnosis")
+
+
 class Visit(Base):
     __tablename__ = "visits"
     id = Column(Integer, primary_key=True)
     diagnosis = relationship("Diagnosis", back_populates="visits")
+
+
 class Disease(Base):
     __tablename__ = 'diseases'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), unique=True, nullable=False)
     effect = Column(REAL, unique=False, default=1.0)
     symptoms = relationship("Symptom", back_populates="diseases")
-    #medication_id = Column(Integer, ForeignKey("medications.id"))
+    # medication_id = Column(Integer, ForeignKey("medications.id"))
     medications = relationship("Medication", back_populates="diseases")
+
     def __init__(self, name=None, effect=None):
         self.effect = effect
         self.name = name
+
+
 class Medication(Base):
     __tablename__ = 'medications'
     id = Column(Integer, primary_key=True)
@@ -94,24 +119,30 @@ class Medication(Base):
     livestock_id = Column(Integer, ForeignKey("livestocks.id"))
     diseases = relationship("Disease", back_populates="medications")
     livestocks = relationship("Livestock", back_populates="medications")
+
     def __init__(self):
         pass
+
+
 class Symptom(Base):
     __tablename__ = "symptoms"
     id = Column(Integer, primary_key=True)
     description = Column(String(1000))
     disease_id = Column(Integer, ForeignKey("diseases.id"))
     diseases = relationship("Disease", back_populates="symptoms")
+
+
 class County(Base):
     __tablename__ = "counties"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
+
     def __init__(self, name=None):
         self.name = name
+
 
 def init_db():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-#init_db()
-
+# init_db()
